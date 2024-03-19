0e8e0605be5bbb5db480c25ebc5cb75ade30a1dc
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-03-19 17:48:50 +0000

    Configure connection to AWS RDS; Create basic static data  models; untrack __pycache__

--- apps/staticdata/models.py
+++ apps/staticdata/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class instrument_class(models.Model):
+    instrument_type = models.CharField(max_length = 25)
+    instrument_class = models.CharField(max_length =50, unique = True)
+
+class equity(models.Model):
+    name = models.CharField(max_length=50, unique = True)
+    instrument_class = models.ForeignKey(instrument_class, on_delete = models.PROTECT)
+
+
+class identifier_type(models.Model):
+    type_name = models.CharField(max_length=25, unique=True)
+
+
+class identifier(models.Model):
+    identifier = models.CharField(max_length=25, unique=True)
+    identifier_type = models.ForeignKey(identifier_type, on_delete=models.PROTECT)
+    instrument = models.PositiveIntegerField
+    instrument_class = models.ForeignKey(instrument_class, on_delete=models.CASCADE)
