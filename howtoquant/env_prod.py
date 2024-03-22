b616757c6176be3f13bd8bef86703d06d179d371
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-03-22 02:41:28 +0000

    Change DB provider, remover connection details

--- howtoquant/env_prod.py
+++
@@ -1,8 +0,0 @@
-production_DB = {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'howtoquant',
-    'USER': 'admin',
-    'PASSWORD': 'fr958Tx*m0',
-    'HOST': 'howtoquant.c5y0oskye8te.eu-north-1.rds.amazonaws.com',
-    'PORT': '3306',
-}
