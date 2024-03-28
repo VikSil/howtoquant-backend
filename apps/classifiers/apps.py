56c27fc219a56363dd082fda8f943a6a67f78955
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-03-28 23:16:03 +0000

    Move classifiers to apps folder

--- apps/classifiers/apps.py
+++ apps/classifiers/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ClassifiersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "classifiers"
+    name = "apps.classifiers"
