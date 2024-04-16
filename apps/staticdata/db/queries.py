3d524df09c90ef3a5e44f0cc1b97250b098d7825
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-04-16 13:09:05 +0100

    Update staticdata queries to not use _id suffixes

--- apps/staticdata/db/queries.py
+++ apps/staticdata/db/queries.py
@@ -19,18 +19,18 @@ equities_with_inst_type_select_all = '''SELECT eq.id, eq.name, inst_class.instru
 
 equities_select_all = '''
     select i.id, i.name, i.short_name, org.short_name as issuer, ccy.ISO as base_ccy, ctry.short_name as domicile from staticdata_equity as eq
-    JOIN staticdata_instrument as i ON eq.instrument_id_id = i.id 
-    JOIN classifiers_currency as ccy ON i.base_ccy_id_id = ccy.id 
-    JOIN classifiers_country as ctry ON i.domicile_id_id = ctry.id
-    JOIN staticdata_organization as org ON i.issuer_id_id = org.id
-    WHERE i.owner_org_id_id = 1
+    JOIN staticdata_instrument as i ON eq.instrument_id = i.id 
+    JOIN classifiers_currency as ccy ON i.base_ccy_id = ccy.id 
+    JOIN classifiers_country as ctry ON i.domicile_id = ctry.id
+    JOIN staticdata_organization as org ON i.issuer_id = org.id
+    WHERE i.owner_org_id = 1
     ;
 '''
 
 identifiers_select_all = '''
     SELECT i.id, i.code, itype.type_name AS type, inst.id AS inst_id, inst.short_name as instrument FROM staticdata_identifier AS i
-    JOIN classifiers_identifier_type AS itype ON i.identifier_type_id_id = itype.id
-    JOIN staticdata_instrument AS inst ON i.instrument_id_id = inst.id
-    WHERE i.owner_org_id_id = 1
+    JOIN classifiers_identifier_type AS itype ON i.identifier_type_id = itype.id
+    JOIN staticdata_instrument AS inst ON i.instrument_id = inst.id
+    WHERE i.owner_org_id = 1
     ;
 '''
