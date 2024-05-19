498e685b3b5daf3cb283859641f6691f7a5a54ad
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-05-19 16:02:16 +0100

    Refactor staticdata endpoints

--- apps/staticdata/urls.py
+++ apps/staticdata/urls.py
@@ -4,10 +4,9 @@ from . import views
 urlpatterns = [
     # Default landing page
     path("", views.index, name="index"),
-    path("api/equities/all", views.all_equities, name="api_all_equities"),
-    path("api/instruments", views.instruments, name="api_instruments"),
-    path("api/instruments/<str:ticker>", views.instrument_by_ticker, name="api_instrument_by_ticker"),
-    path("api/identifiers/all", views.all_identifiers, name="api_all_identifiers"),
+    path("api/equities/<str:ticker>", views.equities, name="api_equities_by_ticker"),
+    path("api/equities", views.equities, name="api_equities"),
     path("api/identifiers/codes", views.all_identifier_codes, name="api_all_identifier_codes"),
-
+    path("api/identifiers", views.identifiers, name="api_identifiers"),
+    path("api/instruments", views.instruments, name="api_instruments"),
 ]
--- apps/staticdata/views.py
+++ apps/staticdata/views.py
@@ -14,9 +14,9 @@ from .db.queries import (
     instruments_select_where_id,
     ticker_select_where_code,
 )
-from .schemas import new_instrument_request
+from .schemas import *
 from .utils_download import get_or_save_organization, save_equity, get_or_save_ticker
-from howtoquant.utils import dict_fetch_all,dict_fetch_one, list_fetch_all, fetch_one_value
+from howtoquant.utils import dict_fetch_all, dict_fetch_one, list_fetch_all, fetch_one_value
 from howtoquant.settings import POLYGON_API_KEY
 
 
@@ -30,26 +30,34 @@ def index(request):
         "index.html",
     )
 
-@api_view(['GET'])
-def all_equities(request):
 
+@api_view(['GET'])
+def equities(request, ticker=None):
     if request.method == 'GET':
-        data = dict_fetch_all(equities_select_all)
-        return JsonResponse({'status': "OK", 'data': {"equities": data}}, safe=False)
+        if ticker == None:  # list of all equities requested
+            data = dict_fetch_all(equities_select_all)
+            return JsonResponse({'status': "OK", 'data': {"equities": data}}, safe=False)
+        else:  # specific equity requested by ticker
+            inst_id = fetch_one_value(ticker_select_where_code, [ticker])
+            if inst_id:
+                data = dict_fetch_one(instruments_select_where_id, [inst_id])
+                return JsonResponse({'status': "OK", 'data': {"instrument_data": data}}, safe=False)
+            else:
+                return HttpResponseBadRequest('Ticker Not Found', status=404)
 
 
 @api_view(['GET'])
-def all_identifiers(request):
-
+def identifiers(request):
     if request.method == 'GET':
         data = dict_fetch_all(identifiers_select_all)
         return JsonResponse({'status': "OK", 'data': {"identifiers": data}}, safe=False)
 
+
 @api_view(['GET'])
 def all_identifier_codes(request):
-
     data = list_fetch_all(identifiers_select_all_codes)
     return JsonResponse({'status': "OK", 'data': {"codes": data}}, safe=False)
+
 
 @api_view(['PUT'])
 def instruments(request):
@@ -66,7 +74,7 @@ def instruments(request):
     if service == 'polygon.io':
         try:
             url = f'https://api.polygon.io/v3/reference/tickers/{ticker}?apiKey={POLYGON_API_KEY}'
-            response =requests.get(url)
+            response = requests.get(url)
             if response.status_code == 200:
                 if response.json()['results']['type'] == 'CS':
                     inst_org = get_or_save_organization(
@@ -84,10 +92,10 @@ def instruments(request):
                     inst_ticker = get_or_save_ticker(ticker, new_cs.instrument_id)
 
                     result = {
-                        "organization":inst_org.id,
+                        "organization": inst_org.id,
                         "instrument_id": new_cs.instrument_id,
                         "equity_id": new_cs.id,
-                        "ticker_id":inst_ticker.id
+                        "ticker_id": inst_ticker.id,
                     }
                     status = 'OK'
                 else:
@@ -107,14 +115,4 @@ def instruments(request):
             result = str(e)
             status = 'NOK'
 
-    return JsonResponse({"data": result, 'status':status}, safe=False)
-
-
-@api_view(['GET'])
-def instrument_by_ticker(request,ticker):
-    inst_id = fetch_one_value(ticker_select_where_code, [ticker])
-    if inst_id:
-        data = dict_fetch_one(instruments_select_where_id, [inst_id])
-        return JsonResponse({'status': "OK", 'data': {"instrument_data": data}}, safe=False)
-    else:
-        return HttpResponseBadRequest('Ticker Not Found', status=404)
+    return JsonResponse({"data": result, 'status': status}, safe=False)
