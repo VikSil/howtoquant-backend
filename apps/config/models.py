394bf820c40c4cfa3083d23a8fffff567f998062
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-08-17 21:44:57 +0100

    Add message queue table

--- apps/config/models.py
+++ apps/config/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+class msg_queue(models.Model):
+    source = models.CharField(max_length=50)
+    source_id = models.BigIntegerField()
+    destination = models.CharField(max_length=50)
+    process = models.CharField(max_length=50)
+    routine = models.CharField(max_length=100, blank=True, null=True)
+    arg1 = models.CharField(max_length=100, blank=True, null=True)
+    arg2 = models.CharField(max_length=100, blank=True, null=True)
+    arg3 = models.CharField(max_length=100, blank=True, null=True)
+    arg4 = models.CharField(max_length=100, blank=True, null=True)
+    created = models.DateTimeField(default=now, blank=True, unique=False)
+    updated = models.DateTimeField(default=now, blank=True, unique=False)
